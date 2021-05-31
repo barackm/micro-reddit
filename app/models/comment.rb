8a8e22fe2e79c1359965345586b605a3164408b7
Barack <barackmukelenga100@gmail.com>
2021-05-31 11:02:07 +0200

    Fix all rubocop errors

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,6 +1,7 @@
 class Comment < ApplicationRecord
-    belongs_to :user 
-    belongs_to :post  
-    
-    validates :body, presence: { message: 'Please enter you comment.' }, length: { minimum: 5, too_short: 'Comments must be longer than 5 characters.' }
+  belongs_to :user
+  belongs_to :post
+
+  validates :body, presence: { message: 'Please enter you comment.' },
+                   length: { minimum: 5, too_short: 'Comments must be longer than 5 characters.' }
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -3,5 +3,6 @@ class Post < ApplicationRecord
   has_many :comments
 
   validates :title, presence: { message: 'Please give a title for your post' }
-  validates :body, presence: { message: 'Please give some information to your post' }, length: { minimum: 10, too_short: 'Posts must be longer than 10 characters' }
+  validates :body, presence: { message: 'Please give some information to your post' },
+                   length: { minimum: 10, too_short: 'Posts must be longer than 10 characters' }
 end

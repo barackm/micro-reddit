308a969d6721239375e99facdaa2078a4a72101c
Barack <barackmukelenga100@gmail.com>
2021-05-31 10:34:09 +0200

    Add the model for the posts and the necessary validations and associations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,7 @@
 class User < ApplicationRecord
+  has_many :posts
+   
   validates :name, presence: { message: 'Please enter you name', minimum: 3, too_short: "Username must be at least 3 characters long." }
-  validates :password, length: { minimum: 8, too_short: 'Password must be at least 8 characters long.' }
+  validates :password, length: { minimum: 4, too_short: 'Password must be at least 4 characters long.' }
   validates :email, uniqueness: { message: 'Email already exists' }
 end
